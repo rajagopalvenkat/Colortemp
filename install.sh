7bdf08051fcf0d1e4ba38a3d44c87e2b52f543c1
rajagopalvenkat <rajagopal.1995@gmail.com>
2015-11-23 16:58:44 +0530

    Update, works for all Ubuntu versions.

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-sudo apt-get install redshift
-sudo apt-get install python-tk
-sudo apt-get install python-pip
-sudo pip install pyinstaller
-pyinstaller ./src/Colortemp.py
+sudo apt-get install gtk-redshift
+echo "Setting up Colortemp"
 sudo mkdir /usr/share/Colortemp
 sudo cp -avr ./* /usr/share/Colortemp
 sudo touch /usr/share/Colortemp/dist/Colortemp/Colortemp.bash
 sudo chmod 777 /usr/share/Colortemp/dist/Colortemp/Colortemp.bash
-sudo cat bashinstruct > /usr/share/Colortemp/dist/Colortemp/Colortemp.bash
+sudo cat bashinstruct.bash > /usr/share/Colortemp/dist/Colortemp/Colortemp.bash
 sudo chmod +x /usr/share/Colortemp/dist/Colortemp/Colortemp.bash
 sudo desktop-file-install ./.Desktop/Colortemp.desktop
 sudo echo "alias colortemp='/usr/share/Colortemp/dist/Colortemp/Colortemp.bash'" >> ~/.bashrc
 source ~/.bashrc
+echo "Colortemp is installed and ready to use."
+echo "Type \"colortemp\" on a terminal, or search your application menu."
